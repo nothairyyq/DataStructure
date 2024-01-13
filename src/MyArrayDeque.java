cdd5fb8f1a0cc7fbb1d19df2ae994951629c8df3
unknown <yaqiyu@uw.edu>
2024-01-13 15:58:29 -0800

    Deque

---
+++ src/MyArrayDeque.java
@@ -0,0 +1,103 @@
+import java.util.NoSuchElementException;
+
+public class MyArrayDeque<E> {
+    private int size;
+    private E[] data;
+    private final static int INIT_CAP = 1;
+
+    private int first, last;
+
+    public MyArrayDeque(int initCap){
+        size = 0;
+        data = (E[]) new Object[initCap];
+        first = last = 0;
+    }
+
+    public MyArrayDeque(){
+        this(INIT_CAP);
+    }
+
+    public void addFirst(E e){
+        if (size == data.length) resize(size*2);
+        if (first == 0)
+            first = data.length-1;
+        else
+            first--;
+        data[first] =e;
+        size++;
+    }
+    public void addLast(E e){
+        if(size == data.length) resize(size*2);
+        data[last] = e;
+        last++;
+        if(last == data.length){
+            last = 0;
+        }
+        size++;
+    }
+
+
+    public E removeFirst(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+        if (size <= data.length / 4)
+            resize(data.length/2);
+        E deleteVal = data[first];
+        data[first] = null;
+        first++;
+        if (first == data.length){
+            first = 0;
+        }
+        size--;
+        return deleteVal;
+    }
+
+    public E removeLast(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+        if (size <= data.length / 4)
+            resize(data.length/2);
+
+        if (last == 0){
+            last = data.length-1;
+        }else{
+            last--;
+        }
+
+        E deleteVal = data[last];
+        data[last] = null;
+        size--;
+        return deleteVal;
+    }
+
+    public E getFirst(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+        return data[first];
+    }
+
+    public E getLast(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+        if (last==0) return data[data.length-1];
+        return data[last-1];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private void resize(int newCap){
+        E[] temp = (E[]) new Object[newCap];
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[(first+i) % data.length];
+        }
+        first = 0;
+        last = size;
+        data = temp;
+    }
+}
