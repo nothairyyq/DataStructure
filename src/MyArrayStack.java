c9991c27cc423c7e27b1ff5043d458c3b84266f5
unknown <yaqiyu@uw.edu>
2024-01-13 14:52:24 -0800

    arrayStack update

--- src/MyArrayStack.java
+++ src/MyArrayStack.java
@@ -7,4 +7,15 @@ public class MyArrayStack<E> {
         arr.addLast(e);
     }
 
+    public E pop(){
+        return arr.removeLast();
+    }
+
+    public E peek(){
+        if (arr.isEmpty())
+            throw new NoSuchElementException();
+        return arr.get(arr.size()-1);
+    }
+
+
 }
